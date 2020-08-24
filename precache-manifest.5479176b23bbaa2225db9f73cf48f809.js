3a04a774f4659b4c92de9095f1b33e8bbb6744dd
Shreyans Jain <jainshreyans121@gmail.com>
2020-08-24 16:50:23 +0530

    Updates

---
+++ precache-manifest.5479176b23bbaa2225db9f73cf48f809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7759e52e8c09d48edffdddb94116b148",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3a20241dd7d72366a4f3",
+    "url": "/monsters-rolodex/static/css/main.9eb51767.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a20241dd7d72366a4f3",
+    "url": "/monsters-rolodex/static/js/main.74fb5c8c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
